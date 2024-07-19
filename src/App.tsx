8c0ccf54624f42129ed03076424a388a5475e9a9
Ali Etebarain <alietebarianbusiness@gmail.com>
2024-07-19 22:19:48 +0330

    start todo project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="TodoWrapper">
+      <h1>Todo List ❤️ </h1>
+      {/* Add New Todo Form */}
+
+      <form className="TodoForm">
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="What is the task today?"
+        />
+        <button type="submit" className="todo-btn">
+          Add Task
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      </form>
+
+      {/* display todos */}
+      <div className="Todo">
+        <p
+          className="" // or completed className
+        >
+          Test Todo Title :))
         </p>
+        <div>{/* <FontAwesomeIcon icon={faTrash} /> */}</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
